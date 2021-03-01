3a7e6c8a8a1a6c1da87c85090df7ac2775d73e57
sourav saha <souravsaha675@gmail.com>
2021-03-02 02:02:55 +0530

    initial commit

---
+++ src/components/users/Users.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import UserItem from "./UserItem";
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: "1",
+        login: "DD",
+        avatar_url: "ffdfdf",
+        html_url: "knfkfn",
+      },
+      {
+        id: "2",
+        login: "DD",
+        avatar_url: "ffdfdf",
+        html_url: "knfkfn",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map((user) => (
+          <UserItem key={user.id} user />
+        ))}
+      </div>
+    );
+  }
+}
+
+const userStyle = {
+  display: "grid",
+  gridTemplateColumns: "repeat(3,1fr)",
+  gridGap: "1rem",
+};
+
+export default Users;
